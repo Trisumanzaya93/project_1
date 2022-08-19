67242b41fe280e7462e6b84a432bbf6adc93659c
Trisumanzaya93 <trisumanzaya93a@gmail.com>
2022-08-19 16:47:14 +0700

    setup notif

---
+++ src/controllers/notif.js
@@ -0,0 +1,68 @@
+const admin = require("firebase-admin");
+const key = require("../../private/pais-8b1c5-firebase-adminsdk-zuwqs-b8209e6c35");
+const model = require("../models/index");
+// const ServiceResponse = require("../helper/ServiceResponse");
+// const models = require("../models/firebase");
+
+// FIREBASE
+admin.initializeApp({
+  credential: admin.credential.cert(key),
+});
+const message = admin.messaging();
+
+const notifSend = async (req,res) => {
+    const {body} = req
+    try {
+         const result = await model.users.findOne({where:{id:body.id}})
+         if (result.length===0) return res.status(404).json({pesan:"id tidak ditemukan"})
+         const token = result.id_android
+        await message.send({
+            token: token,
+            notification: {
+                title: body.title,
+                body: body.message,
+            },
+        })
+        return res.status(200).json({
+                  pesan: "notification sent",
+                  token
+                });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+                  pesan: "firebase-server",
+                  error,
+                });
+    }
+}
+
+const notifSendAllUser = async (req,res) => {
+    const {body} = req
+    try {
+         const result = await model.users.findAll({where:{id_admin:body.id}})
+         if (result.length===0) return res.status(404).json({pesan:"id tidak ditemukan"})
+         await Promise.all(
+         result.map( async(item)=>{
+             const token = item.id_android
+            return await message.send({
+                token: token,
+                notification: {
+                    title: body.title,
+                    body: body.message,
+                },
+            })
+         }))
+        return res.status(200).json({
+                  pesan: "notification sent",
+                //   token
+                });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+                  pesan: "firebase-server",
+                  error,
+                });
+    }
+}
+
+module.exports={notifSend, notifSendAllUser}
--- src/routers/admin.js
+++ src/routers/admin.js
@@ -2,9 +2,12 @@
 const express = require('express')
 const admin = express.Router()
 const adminController = require('./../controllers/admin')
+const notifController = require("../controllers/notif")
 
 admin
         .post('/loginadmin',  adminController.loginAdmin)
+        .post('/notif',  notifController.notifSend)
+        .post('/notifalluser',  notifController.notifSendAllUser)
         .get('/:id_admin',  adminController.getUserByAdmin)
         .post('/',  adminController.createUser)
         .patch('/:id', adminController.updateUser)
